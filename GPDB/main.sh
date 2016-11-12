87c3599060a8bac22e282ad03c73418cbc7281e4
pengji <jipeng92@gmail.com>
2016-11-12 22:51:16 +0800

    modified:   GPDB/main.sh

--- GPDB/main.sh
+++ GPDB/main.sh
@@ -7,64 +7,54 @@ doc
 echo -e "\033[35;49;1m [program start] \033[39;49;0m"
 echo "************************************program start**************************************" >> run.log
 
+# 普通表分别执行10次
 truncateTableFun
 dropTableFun
-
-# 普通表分别执行10次
 createTableFun galaxy
 createTableFun photoobjall 
 createTableFun photoprimary
 createTableFun Star
 createTableFun neighbors
-
 mainFun
 
+# appendonly表分别执行10次
 truncateTableFun
 dropTableFun
-
-# appendonly表分别执行10次
 createTableFun galaxy a
 createTableFun photoobjall a
 createTableFun photoprimary a
 createTableFun Star a
 createTableFun neighbors
-
 mainFun a
 
+# 压缩表分别执行10次
 truncateTableFun
 dropTableFun
-
-# 压缩表分别执行10次
 createTableFun galaxy ac
 createTableFun photoobjall ac
 createTableFun photoprimary ac
 createTableFun Star ac
 createTableFun neighbors
-
 mainFun ac
 
+# 列存储表分别执行10次
 truncateTableFun
 dropTableFun
-
-# 列存储表分别执行10次
 createTableFun galaxy ao
 createTableFun photoobjall ao 
 createTableFun photoprimary ao
 createTableFun Star ao
 createTableFun neighbors
-
 mainFun ao
 
+# 列存储压缩表分别执行10次
 truncateTableFun
 dropTableFun
-
-# 列存储压缩表分别执行10次
 createTableFun galaxy aoc
 createTableFun photoobjall aoc
 createTableFun photoprimary aoc
 createTableFun Star aoc
 createTableFun neighbors
-
 mainFun aoc
 
 # 清理数据库和文件
