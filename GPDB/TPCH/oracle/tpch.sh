8fbc14b48e9c258693ce1c2b0ab4c9d0d08156a1
pengji <jipeng92@gmail.com>
2017-03-06 21:14:30 +0800

    #3 greenplum tpch modified tpch.sh

--- GPDB/TPCH/oracle/tpch.sh
+++ GPDB/TPCH/oracle/tpch.sh
@@ -14,29 +14,29 @@ DSS_TIMEOUT=300000 # 5 minutes in seconds
 LOGFILE=bench.log
 
 function benchmark_run() {
-	mkdir -p $RESULTS
+	mkdir -p "$RESULTS"
 	# store the settings
-	psql -h localhost postgres -c "select name,setting from pg_settings" > $RESULTS/settings.log 2> $RESULTS/settings.err
+	psql postgres -c "select name,setting from pg_settings" > $RESULTS/settings.log 2> $RESULTS/settings.err
 	print_log "preparing TPC-H database"
 	# create database, populate it with data and set up foreign keys
 	#psql -h localhost $DBNAME < dss/tpch-create.sql > $RESULTS/create.log 2> $RESULTS/create.err
 	print_log "  loading data"
-	psql -h localhost -U $USER $DBNAME < dss/tpch-load.sql > $RESULTS/load.log 2> $RESULTS/load.err
+	psql $DBNAME < dss/tpch-load.sql > $RESULTS/load.log 2> $RESULTS/load.err
 	print_log "  creating primary keys"
-	psql -h localhost -U $USER $DBNAME < dss/tpch-pkeys.sql > $RESULTS/pkeys.log 2> $RESULTS/pkeys.err
+	psql $DBNAME < dss/tpch-pkeys.sql > $RESULTS/pkeys.log 2> $RESULTS/pkeys.err
 	print_log "  creating foreign keys"
-	psql -h localhost -U $USER $DBNAME < dss/tpch-alter.sql > $RESULTS/alter.log 2> $RESULTS/alter.err
+	psql $DBNAME < dss/tpch-alter.sql > $RESULTS/alter.log 2> $RESULTS/alter.err
 	print_log "  creating indexes"
-	psql -h localhost -U $USER $DBNAME < dss/tpch-index.sql > $RESULTS/index.log 2> $RESULTS/index.err
+	psql $DBNAME < dss/tpch-index.sql > $RESULTS/index.log 2> $RESULTS/index.err
 	print_log "  analyzing"
-	psql -h localhost -U $USER $DBNAME -c "analyze" > $RESULTS/analyze.log 2> $RESULTS/analyze.err
+	psql $DBNAME -c "analyze" > $RESULTS/analyze.log 2> $RESULTS/analyze.err
 	print_log "running TPC-H benchmark"
 	benchmark_dss $RESULTS
 	print_log "finished TPC-H benchmark"
 }
 
 function benchmark_dss() {
-	mkdir -p $RESULTS
+	mkdir -p "$RESULTS"
 	mkdir $RESULTS/vmstat-s $RESULTS/vmstat-d $RESULTS/explain $RESULTS/results $RESULTS/errors
 	# get bgwriter stats
 	psql postgres -c "SELECT * FROM pg_stat_bgwriter" > $RESULTS/stats-before.log 2>> $RESULTS/stats-before.err
@@ -49,20 +49,20 @@ function benchmark_dss() {
 
 	for n in `seq 1 22`
 	do
-		q="dss/queries/$n.sql"
-		qe="dss/queries/$n.explain.sql"
+		q="dss/sql/$n.sql"
+		qe="dss/sql/$n.explain.sql"
 
 		if [ -f "$q" ]; then
 			print_log "  running query $n"
 			echo "======= query $n =======" >> $RESULTS/data.log 2>&1;
 			# run explain
-			psql -h localhost -U $USER $DBNAME < $qe > $RESULTS/explain/$n 2>> $RESULTS/explain.err
+			psql $DBNAME < $qe > $RESULTS/explain/$n 2>> $RESULTS/explain.err
 
 			vmstat -s > $RESULTS/vmstat-s/before-$n.log 2>&1
 			vmstat -d > $RESULTS/vmstat-d/before-$n.log 2>&1
 
 			# run the query on background
-			/usr/bin/time -a -f "$n = %e" -o $RESULTS/results.log psql -h localhost -U $USER $DBNAME < $q > $RESULTS/results/$n 2> $RESULTS/errors/$n &
+			/usr/bin/time -a -f "$n = %e" -o $RESULTS/results.log psql $DBNAME < $q > $RESULTS/results/$n 2> $RESULTS/errors/$n &
 
 			# wait up to the given number of seconds, then terminate the query if still running (don't wait for too long)
 			for i in `seq 0 $DSS_TIMEOUT`
@@ -73,11 +73,11 @@ function benchmark_dss() {
 					if [ $i -eq $DSS_TIMEOUT ]; then
 						print_log "    killing query $n (timeout)"
 						# echo "$q : timeout" >> $RESULTS/results.log
-						psql -h localhost postgres -c "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'tpch'" >> $RESULTS/queries.err 2>&1;
+						psql postgres -c "SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE datname = 'tpch'" >> $RESULTS/queries.err 2>&1;
 						# time to do a cleanup
 						sleep 10;
 						# just check how many backends are there (should be 0)
-						psql -h localhost postgres -c "SELECT COUNT(*) AS tpch_backends FROM pg_stat_activity WHERE datname = 'tpch'" >> $RESULTS/queries.err 2>&1;
+						psql postgres -c "SELECT COUNT(*) AS tpch_backends FROM pg_stat_activity WHERE datname = 'tpch'" >> $RESULTS/queries.err 2>&1;
 					else
 						# the query is still running and we have time left, sleep another second
 						sleep 1;
@@ -132,7 +132,7 @@ function print_log() {
 
 }
 
-mkdir $RESULTS;
+mkdir "$RESULTS";
 
 # start statistics collection
 stat_collection_start $RESULTS
