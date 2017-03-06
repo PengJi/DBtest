0672e00132f20f1c9ec89598bbaae64fceddff34
pengji <jipeng92@gmail.com>
2017-03-06 16:56:58 +0800

    #3 greenplum tpch test

---
+++ GPDB/TPCH/oracle/dss/tpch-create.sql
@@ -0,0 +1,101 @@
+drop table if exists PART;
+CREATE TABLE PART (
+
+	P_PARTKEY		SERIAL,
+	P_NAME			VARCHAR(55),
+	P_MFGR			CHAR(25),
+	P_BRAND			CHAR(10),
+	P_TYPE			VARCHAR(25),
+	P_SIZE			INTEGER,
+	P_CONTAINER		CHAR(10),
+	P_RETAILPRICE	DECIMAL,
+	P_COMMENT		VARCHAR(23)
+)with(appendonly=true,compresslevel=5)
+distributed by(P_PARTKEY);
+
+drop table if exists SUPPLIER;
+CREATE TABLE SUPPLIER (
+	S_SUPPKEY		SERIAL,
+	S_NAME			CHAR(25),
+	S_ADDRESS		VARCHAR(40),
+	S_NATIONKEY		INTEGER NOT NULL, -- references N_NATIONKEY
+	S_PHONE			CHAR(15),
+	S_ACCTBAL		DECIMAL,
+	S_COMMENT		VARCHAR(101)
+)with(appendonly=true,compresslevel=5)
+distributed by(S_SUPPKEY);
+
+drop table if exists PARTSUPP;
+CREATE TABLE PARTSUPP (
+	PS_PARTKEY		INTEGER NOT NULL, -- references P_PARTKEY
+	PS_SUPPKEY		INTEGER NOT NULL, -- references S_SUPPKEY
+	PS_AVAILQTY		INTEGER,
+	PS_SUPPLYCOST	DECIMAL,
+	PS_COMMENT		VARCHAR(199)
+)with(appendonly=true,compresslevel=5)
+distributed by(PS_PARTKEY);
+
+drop table if exists CUSTOMER;
+CREATE TABLE CUSTOMER (
+	C_CUSTKEY		SERIAL,
+	C_NAME			VARCHAR(25),
+	C_ADDRESS		VARCHAR(40),
+	C_NATIONKEY		INTEGER NOT NULL, -- references N_NATIONKEY
+	C_PHONE			CHAR(15),
+	C_ACCTBAL		DECIMAL,
+	C_MKTSEGMENT	CHAR(10),
+	C_COMMENT		VARCHAR(117)
+)with(appendonly=true,compresslevel=5)
+distributed by(C_CUSTKEY);
+
+drop table if exists ORDERS;
+CREATE TABLE ORDERS (
+	O_ORDERKEY		SERIAL,
+	O_CUSTKEY		INTEGER NOT NULL, -- references C_CUSTKEY
+	O_ORDERSTATUS	CHAR(1),
+	O_TOTALPRICE	DECIMAL,
+	O_ORDERDATE		DATE,
+	O_ORDERPRIORITY	CHAR(15),
+	O_CLERK			CHAR(15),
+	O_SHIPPRIORITY	INTEGER,
+	O_COMMENT		VARCHAR(79)
+)with(appendonly=true,compresslevel=5)
+distributed by(O_ORDERKEY);
+
+drop table if exists LINEITEM;
+CREATE TABLE LINEITEM (
+	L_ORDERKEY		INTEGER NOT NULL, -- references O_ORDERKEY
+	L_PARTKEY		INTEGER NOT NULL, -- references P_PARTKEY (compound fk to PARTSUPP)
+	L_SUPPKEY		INTEGER NOT NULL, -- references S_SUPPKEY (compound fk to PARTSUPP)
+	L_LINENUMBER	INTEGER,
+	L_QUANTITY		DECIMAL,
+	L_EXTENDEDPRICE	DECIMAL,
+	L_DISCOUNT		DECIMAL,
+	L_TAX			DECIMAL,
+	L_RETURNFLAG	CHAR(1),
+	L_LINESTATUS	CHAR(1),
+	L_SHIPDATE		DATE,
+	L_COMMITDATE	DATE,
+	L_RECEIPTDATE	DATE,
+	L_SHIPINSTRUCT	CHAR(25),
+	L_SHIPMODE		CHAR(10),
+	L_COMMENT		VARCHAR(44)
+)with(appendonly=true,compresslevel=5)
+distributed by(L_ORDERKEY);
+
+drop table if exists NATION;
+CREATE TABLE NATION (
+	N_NATIONKEY		SERIAL,
+	N_NAME			CHAR(25),
+	N_REGIONKEY		INTEGER NOT NULL,  -- references R_REGIONKEY
+	N_COMMENT		VARCHAR(152)
+)with(appendonly=true,compresslevel=5)
+distributed by(N_NATIONKEY);
+
+drop table if exists REGION;
+CREATE TABLE REGION (
+	R_REGIONKEY	SERIAL,
+	R_NAME		CHAR(25),
+	R_COMMENT	VARCHAR(152)
+)with(appendonly=true,compresslevel=5)
+distributed by(R_REGIONKEY);
