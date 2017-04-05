231c21184af422023111766cd8d9e78fdf215ff8
pengji <jipeng92@gmail.com>
2017-04-05 20:07:03 +0800

    #3 modified Q8.sql

--- GPDB/python/queries/Q8.sql
+++ GPDB/python/queries/Q8.sql
@@ -1,8 +1,26 @@
 -- Find all objects with unclassified spectra.  
 
+/*
  declare unknown bigint;			 	-- initialized “binned” literal
  set    unknown = dbo.fSpecClass('UNKNOWN')  
  select specObjID
  from   SpecObj
  where  SpecClass = unknown;
+*/
 
+declare unknown bigint
+unknown := fSpecClass('UNKNOWN')  
+select specObjID
+from   SpecObj
+where  SpecClass = unknown;
+
+/*
+tables:
+specObjAll
+
+views:
+SpecObj
+
+functions:
+
+*/
