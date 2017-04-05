f24dcf3bb897a90bb2be5351f03f3bf603d9945a
pengji <jipeng92@gmail.com>
2017-04-05 22:08:48 +0800

    #3 modified Q1.sql

--- GPDB/python/queries/Q1.sql
+++ GPDB/python/queries/Q1.sql
@@ -1,9 +1,28 @@
 -- Find all galaxies without saturated pixels within 1' of a given point.
 
+create or replace function fQ1()
+returns bigint
+as $$
 declare saturated bigint;
-set saturated = fPhotoFlags('saturated');
+begin
+saturated := fPhotoFlags('saturated');
 select G.objID, GN.distance
 from Galaxy as G join fGetNearbyObjEq(185,-0.5, 1) as GN
 on G.objID = GN.objID
 where (G.flags & saturated) = 0
 order by distance;
+end;
+$$ language plpgsql;
+
+/*
+table:
+DataConstants
+
+view:
+
+function: 
+fPhotoFlags
+fGetNearbyObjEq
+fGetNearbyObjXYZ
+fHtmCoverCircleXyz
+*/
