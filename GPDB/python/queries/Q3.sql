4845365caf0832ef5f806d698bf1b05e3f55d607
pengji <jipeng92@gmail.com>
2017-04-05 19:21:19 +0800

    #3 modified Q3.sql

--- GPDB/python/queries/Q3.sql
+++ GPDB/python/queries/Q3.sql
@@ -1,7 +1,24 @@
 -- Find all galaxies brighter than magnitude 22, where the local extinction is >0.175.
-
-explain analyze
+/*
 select objID
 from Galaxy
 where r < 22
 and reddening_r> 0.175;
+*/
+
+-- 输出结果到文件
+\o /tmp/Q3.txt
+
+create or replace function fQ3()
+returns setof text
+as $$
+declare rho float;
+begin
+return query explain analyze select objID
+	from Galaxy
+	where r < 22
+	and dered_r> 0.175;
+end;
+$$ language plpgsql;
+
+select fQ3();
