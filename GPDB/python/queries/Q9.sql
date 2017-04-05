4731df0a2817a06ba2878ea83028f6407fe66c5c
pengji <jipeng92@gmail.com>
2017-04-05 20:14:34 +0800

    #3 modified Q9.sql

--- GPDB/python/queries/Q9.sql
+++ GPDB/python/queries/Q9.sql
@@ -1,5 +1,6 @@
 -- Find quasars with a line width >2000 km/s and 2.5<redshift<2.7. 
 
+/*
 declare		qso     int;
 set		qso = dbo.fSpecClass('QSO') ;
 declare		hiZ_qso int;
@@ -15,4 +16,28 @@ where  s.specObjID=l.specObjID 		-- line belongs to spectrum of this obj
    and  l.sigma*300000.0/l.wave >2000.0	-- convert sigma to km/s         
    and  s.zConf > 0.9                     	-- high confidence on redshift estimate 
 group by s.specObjID;
+*/
 
+declare qso int;
+qso := fSpecClass('QSO') ;
+declare hiZ_qso int;
+hiZ_qso := fSpecClass('HIZ-QSO');
+select s.specObjID,max(l.sigma*300000.0/l.wave) as veldisp,avg(s.z) as z
+from SpecObj s, specLine l     
+where  s.specObjID=l.specObjID 		
+   and ((s.specClass = qso) or (s.specClass = hiZ_qso))  
+   and  s.z between 2.5 and 2.7   
+   and  l.sigma*300000.0/l.wave >2000.0	         
+   and  s.zConf > 0.9        
+group by s.specObjID;
+
+/*
+tables:
+specObjAll
+
+views:
+SpecObj
+
+functions:
+fSpecClass
+*/
