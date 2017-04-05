266cf9f6de8c5a1ccb4d0d644446810c081e38c4
pengji <jipeng92@gmail.com>
2017-04-05 16:57:52 +0800

    Q2.sql

--- GPDB/python/queries/Q2.sql
+++ GPDB/python/queries/Q2.sql
@@ -8,21 +8,18 @@ and dec < 0
 and g+rho between 23 and 25;
 */
 
-\w /tmp/test.txt
+-- 输出结果到文件
+\o /tmp/test.txt
 
 create or replace function fQ2()
 returns setof text
 as $$
 declare rho float;
-declare str text;
 begin
-	-- select -5*log(R)-2.5*log (PI()) into rho from Galaxy;
-	perform '\o /tmp/test.txt';
 	for rho in execute
-		'select -5*log(R)-2.5*log (PI()) from Galaxy;'
+		'select -5*log(R)-2.5*log(PI()) from Galaxy;'
 	loop
 		raise notice '%', rho;
-		-- execute 'select now()';
 		return query explain analyze select objID
 		from Galaxy
 		where ra between 170 and 190
