db9e54fc2534071dd9dd97bf51e959cbc6bb5099
pengji <jipeng92@gmail.com>
2017-04-06 10:37:52 +0800

    #3 modified queries

--- GPDB/python/queries/Q17.sql
+++ GPDB/python/queries/Q17.sql
@@ -1,5 +1,5 @@
 -- Find binary stars where at least one of them has the colors of a white dwarf. 
-
+/*
 declare star int;			    	-- initialized “star” literal
 set     star = dbo.fPhotoType('Star'); 	-- avoids SQL2K optimizer problem
 select	s1.objID as s1, s2.objID as s2 	-- return star pairs
@@ -14,4 +14,39 @@ from	Star       S1,				-- S1 is the white dwarf
     and (S1.g - S1.r) < 0.7 			-- white dwarfs.
     and (S1.r - S1.i) > 0.4 
     and (S1.i - S1.z) > 0.4;
+*/
+\o /tmp/Q17.txt
 
+create or replace function fQ17()
+returns setof text
+as $$
+declare star1 int;			
+begin
+star1 := fPhotoType('Star'); 
+return query explain analyze 
+select s1.objID as s1, s2.objID as s2 
+from Star S1,		
+   	Neighbors N,				
+   	Star S2	
+  where S1.objID = N. objID 		
+    and S2.objID = N.NeighborObjID  
+    and N.NeighborObjType = star1	
+	and N.DistanceMins < .05			
+    and (S1.u - S1.g) < 0.4  
+	and (S1.g - S1.r) < 0.7
+	and (S1.r - S1.i) > 0.4 
+    and (S1.i - S1.z) > 0.4;
+end;
+$$ language plpgsql;
+
+select fQ17();
+
+/*
+tables:
+
+views:
+PhotoType
+
+functions:
+
+*/
--- GPDB/python/queries/Q18.sql
+++ GPDB/python/queries/Q18.sql
@@ -1,5 +1,5 @@
 -- Find all objects within 30 arcseconds of one another that have very similar colors: that is where the color ratios u-g, g-r, r-i are less than 0.05m.   
-
+/*
  select	 distinct P.ObjID 			-- count distinct cases (will get min objid)
   From	photoPrimary   P,			-- P is the primary object
 Neighbors      N, 			-- N is the neighbor link
@@ -11,3 +11,27 @@ photoPrimary   L			-- L is the lens candidate of P
    and abs((P.g-P.r)-(L.g-L.r))<0.05
    and abs((P.r-P.i)-(L.r-L.i))<0.05  
    and abs((P.i-P.z)-(L.i-L.z))<0.05;
+*/
+
+\o /tmp/Q18.txt
+
+create or replace function fQ18()
+returns setof text
+as $$
+begin
+return query explain analyze 
+select distinct P.ObjID 
+From photoPrimary P,
+	Neighbors  N, 		
+	photoPrimary L		
+where P.ObjID = N.ObjID			
+	and L.ObjID = N.NeighborObjID  	
+	and P.ObjID < L.ObjID 			
+	and abs((P.u-P.g)-(L.u-L.g))<0.05
+	and abs((P.g-P.r)-(L.g-L.r))<0.05
+	and abs((P.r-P.i)-(L.r-L.i))<0.05  
+	and abs((P.i-P.z)-(L.i-L.z))<0.05;
+end;
+$$ language plpgsql;
+
+select fQ18();
