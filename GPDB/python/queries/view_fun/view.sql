4d00fbbaa5745293a4552bcce8b51c5bb86a15a7
pengji <jipeng92@gmail.com>
2017-04-06 09:10:07 +0800

    #3 add view.sql

---
+++ GPDB/python/queries/view_fun/view.sql
@@ -0,0 +1,66 @@
+--
+CREATE VIEW PhotoPrimary 
+----------------------------------------------------------------------
+--/H These objects are the primary survey objects. 
+--
+--/T Each physical object 
+--/T on the sky has only one primary object associated with it. Upon 
+--/T subsequent observations secondary objects are generated. Since the 
+--/T survey stripes overlap, there will be secondary objects for over 10% 
+--/T of all primary objects, and in the southern stripes there will be a 
+--/T multitude of secondary objects for each primary (i.e. reobservations). 
+----------------------------------------------------------------------
+AS
+SELECT * FROM PhotoObjAll WITH(NOLOCK)
+    WHERE mode=1;
+
+--
+CREATE VIEW Galaxy
+---------------------------------------------------------------
+--/H The objects classified as galaxies from PhotoPrimary.
+--
+--/T The Galaxy view contains the photometric parameters (no
+--/T redshifts or spectroscopic parameters) measured for
+--/T resolved primary objects.
+---------------------------------------------------------------
+AS
+SELECT * 
+    FROM PhotoPrimary
+    WHERE type = 3;
+
+--
+CREATE VIEW PhotoObj
+----------------------------------------------------------------------
+--/H All primary and secondary objects in the PhotoObjAll table, which contains all the attributes of each photometric (image) object. 
+--
+--/T It selects PhotoObj with mode=1 or 2.
+----------------------------------------------------------------------
+AS
+SELECT * FROM PhotoObjAll WITH(NOLOCK)
+	WHERE mode in (1,2);
+
+--
+CREATE VIEW Star
+--------------------------------------------------------------
+--/H The objects classified as stars from PhotoPrimary
+--
+--/T The Star view essentially contains the photometric parameters
+--/T (no redshifts or spectroscopic parameters) for all primary
+--/T point-like objects, including quasars.
+--------------------------------------------------------------
+AS
+SELECT * 
+    FROM PhotoPrimary
+    WHERE type = 6;
+
+--
+CREATE VIEW PhotoType
+------------------------------------------
+--/H Contains the PhotoType enumerated values from DataConstants as int
+------------------------------------------
+AS
+SELECT name, 
+	cast(value as int) as value, 
+	description
+    FROM DataConstants
+    WHERE field='PhotoType' AND name != '';
