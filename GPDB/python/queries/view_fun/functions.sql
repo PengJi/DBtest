eec44c43b62f3dff29820b21204f9c544541d9eb
pengji <jipeng92@gmail.com>
2017-04-09 15:44:28 +0800

    #3 modified queries/view_fun/functions.sql

--- GPDB/python/queries/view_fun/functions.sql
+++ GPDB/python/queries/view_fun/functions.sql
@@ -230,3 +230,286 @@ begin
 end;
 $$ language plpgsql;
 
+create or replace function fQ2()
+returns setof text
+as $$
+declare rho float;
+begin
+    for rho in execute
+        'select -5*log(R)-2.5*log(PI()) from Galaxy;'
+    loop
+        raise notice '%', rho;
+        return query explain analyze select objID
+        from Galaxy
+        where ra between 170 and 190
+        and dec < 0
+        and g+rho between 23 and 25;
+    end loop;
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ3()
+returns setof text
+as $$
+declare rho float;
+begin
+return query explain analyze select objID
+    from Galaxy
+    where r < 22
+    and dered_r> 0.175;
+end;
+$$ language plpgsql;
+
+create or replace function fQ4()
+returns setof text
+as $$
+declare rho float;
+begin
+    for rho in execute
+        'select -5*log(R)-2.5*log(PI()) from Galaxy;'
+    loop
+        raise notice '%', rho;
+        return query explain analyze select objID
+        from Galaxy
+        where r + rho < 24
+        and (power(q_r,2) + power(u_r,2)) > 0.25;
+    end loop;
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ5()
+returns setof text
+as $$
+declare binned bigint;
+declare blended bigint;
+declare noDeBlend bigint;
+declare child bigint;
+declare edge bigint;
+declare saturated bigint;
+begin
+binned := fPhotoFlags('BINNED1') +
+             fPhotoFlags('BINNED2') +
+             fPhotoFlags('BINNED4') ;
+blended := fPhotoFlags('BLENDED');
+noDeBlend := fPhotoFlags('NODEBLEND');
+child := fPhotoFlags('CHILD');
+edge := fPhotoFlags('EDGE');
+saturated := fPhotoFlags('SATURATED');
+    return query explain analyze select objID
+    from Galaxy as G
+    where (G.flags & binned) > 0
+    and (G.flags & ( blended + noDeBlend + child)) != blended
+    and (G.flags & (edge + saturated)) = 0
+    and (G.petroMag_i > 17.5)
+    and (G.petroMag_r > 15.5 OR G.petroR50_r > 2)
+    and (G.petroMag_r < 30 and G.g < 30 and G.r < 30 and G.i < 30)
+    and (( G.r - G.i - (G.g - G.r)/4 - 0.18) BETWEEN -0.2 AND  0.2 )
+    and (( G.r - G.i -(G.g - G.r)/4 -.18) > (0.45 - 4*( G.g - G.r)))
+    and ((G.g - G.r) > ( 1.35 + 0.25 *(G.r - G.i)));
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ6()
+returns setof text
+as $$
+declare rho float;
+begin
+    return query explain analyze
+    select G.ObjID, G.u, G.g, G.r, G.i, G.z
+    from galaxy G, star S
+    where G.parentID > 0
+    and G.parentID = S.parentID;
+end;
+$$ language plpgsql;
+
+create or replace function fQ7()
+returns setof text
+as $$
+declare rho float;
+begin
+        return query explain analyze 
+        select  cast(round(u-g) as int) as UG,     
+    cast(round(g-r) as int) as GR,     
+    cast(round(r-i) as int) as RI,     
+    cast(round(i-z) as int) as IZ,
+    count(*)                    as pop 
+        from  star
+        where (u+g+r+i+z) < 150   
+        group by cast(round(u-g) as int), cast(round(g-r) as int),        
+    cast(round(r-i) as int), cast(round(i-z) as int)
+        order by count(*);
+end;
+$$ language plpgsql;
+
+create or replace function fQ13()
+returns setof text
+as $$
+        declare RightShift12 bigint;
+begin
+        RightShift12 := power(2,24);
+        return query explain analyze 
+        select (htmID /RightShift12) as htm_8, avg(ra) as ra, 
+        avg(dec) as dec, count(*) as pop
+        from Galaxy
+        where (0.7*u - 0.5*g - 0.2*i) < 1.25 and  r < 21.75
+        group by (htmID /RightShift12);
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ14()
+returns setof text
+as $$
+declare star1 int;
+begin
+star1 := fPhotoType('Star'); 
+return query explain analyze 
+select s1.objID as ObjID1, s2.objID as ObjID2
+from   star      as   s1,
+       photoObj  as   s2,
+       neighbors as   N
+where s1.objID = N.objID
+  and s2.objID = N.neighborObjID
+  and s1.run != s2.run
+  and s2.type = star1
+  and  s1.u between 1 and 27
+  and  s1.g between 1 and 27
+  and  s1.r between 1 and 27
+  and  s1.i between 1 and 27
+  and  s1.z between 1 and 27
+  and  s2.u between 1 and 27
+  and  s2.g between 1 and 27
+  and  s2.r between 1 and 27
+  and  s2.i between 1 and 27
+  and  s2.z between 1 and 27
+  and (                                
+           abs(S1.u-S2.u) > .1 + (abs(S1.Err_u) + abs(S2.Err_u))  
+        or abs(S1.g-S2.g) > .1 + (abs(S1.Err_g) + abs(S2.Err_g)) 
+        or abs(S1.r-S2.r) > .1 + (abs(S1.Err_r) + abs(S2.Err_r)) 
+        or abs(S1.i-S2.i) > .1 + (abs(S1.Err_i) + abs(S2.Err_i)) 
+        or abs(S1.z-S2.z) > .1 + (abs(S1.Err_z) + abs(S2.Err_z)) 
+        );
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ15()
+returns setof text
+as $$
+begin
+return query explain analyze select objID, 
+        sqrt( power(rowv,2) + power(colv, 2) ) as velocity,
+        fGetUrlExpId(objID) as Url
+ from PhotoObj  
+ where (power(rowv,2) + power(colv, 2)) between 50 and 1000      
+   and rowv >= 0 and colv >=0;
+end;
+$$ language plpgsql;
+
+
+create or replace function fQ16()
+returns setof text
+as $$
+begin
+return query explain analyze select count(*) as total,
+sum( case when (type=3) then 1 else 0 end) as Galaxies,
+sum( case when (type=6) then 1 else 0 end) as Stars,
+sum( case when (type not in (3,6)) then 1 else 0 end) as Other
+from PhotoPrimary                                        
+where (( u - g > 2.0) or (u > 22.3) ) 
+        and ( i between 0 and 19 ) 
+        and ( g - r > 1.0 ) 
+        and ( (r - i < 0.08 + 0.42 * (g - r - 0.96)) or (g - r > 2.26 ) )
+        and ( i - z < 0.25 );
+end;
+$$ language plpgsql;
+
+create or replace function fQ17()
+returns setof text
+as $$
+declare star1 int;
+begin
+star1 := fPhotoType('Star'); 
+return query explain analyze 
+select s1.objID as s1, s2.objID as s2 
+from Star S1,
+        Neighbors N,
+        Star S2
+  where S1.objID = N. objID 
+    and S2.objID = N.NeighborObjID  
+    and N.NeighborType = star1
+        and N.Distance < .05
+    and (S1.u - S1.g) < 0.4  
+        and (S1.g - S1.r) < 0.7
+        and (S1.r - S1.i) > 0.4 
+    and (S1.i - S1.z) > 0.4;
+end;
+$$ language plpgsql;
+
+create or replace function fQ18()
+returns setof text
+as $$
+begin
+return query explain analyze 
+select distinct P.ObjID 
+From photoPrimary P,
+        Neighbors  N, 
+        photoPrimary L
+where P.ObjID = N.ObjID
+        and L.ObjID = N.NeighborObjID  
+        and P.ObjID < L.ObjID 
+        and abs((P.u-P.g)-(L.u-L.g))<0.05
+        and abs((P.g-P.r)-(L.g-L.r))<0.05
+        and abs((P.r-P.i)-(L.r-L.i))<0.05  
+        and abs((P.i-P.z)-(L.i-L.z))<0.05;
+end;
+$$ language plpgsql;
+
+create or replace function fQ20()
+returns setof text
+as $$
+declare binned          bigint;                         -- initialized “binned” literal
+declare blended         bigint;                         -- initialized “blended” literal
+declare noDeBlend       bigint;                         -- initialized “noDeBlend” literal
+declare child           bigint;                         -- initialized “child” lit-eral
+declare edge            bigint;                         -- initialized “edge” lit-eral
+declare saturated       bigint;                         -- initialized “saturated” literal
+begin
+binned := fPhotoFlags('BINNED1') +      -- avoids SQL2K optimizer problem
+                fPhotoFlags('BINNED2') +
+                fPhotoFlags('BINNED4') ;
+blended := fPhotoFlags('BLENDED');      -- avoids SQL2K optimizer problem
+noDeBlend := fPhotoFlags('NODEBLEND'); -- avoids SQL2K optimiz-er problem
+child = fPhotoFlags('CHILD');   -- avoids SQL2K optimizer problem
+edge := fPhotoFlags('EDGE');    -- avoids SQL2K optimizer problem
+saturated := fPhotoFlags('SATURATED'); -- avoids SQL2K optimizer prob-lem
+return query explain analyze select  G.objID, count(*) as pop
+from Galaxy     as G,                   -- first gravitational lens candidate   
+        Neighbors  as N,                        -- precomputed list of neighbors
+        Galaxy     as U,                        -- a neighbor galaxy of G
+        PhotoZ     as GpZ,                      -- photoZ of first galaxy
+        PhotoZ     as NpZ                       -- photoZ of second galaxy
+where  G.objID = N.objID                -- connect G and U via the neighbors table
+   and U.objID = N.neighborObjID        -- so that we know G and U are within 
+   and N.objID < N.neighborObjID        -- 30 arcseconds of one another.
+   and G.objID = GpZ.objID              -- join to photoZ of G
+   and U.objID = NpZ.objID              -- join to photoZ of N
+   and G.ra between 160 and 170         -- restrict search to a part of the sky
+   and G.dec between -5 and 5           -- that is in database
+   and abs(GpZ.Z - NpZ.Z) < 0.05        -- restrict the photoZ differences
+   and (G.flags & binned) > 0  
+   and (G.flags & ( blended + noDeBlend + child)) != blended
+   and (G.flags & (edge + saturated)) = 0  
+   and  G.petroMag_i > 17.5
+   and (G.petroMag_r > 15.5 or G.petroR50_r > 2)
+   and (G.g >0 and G.r >0 and G.i >0)
+   and ((abs( G.r - G.i - (G.g - G.r )/4 - 0.18 )) < 0.2)
+          and ((G.r - G.i - (G.g - G.r)/4 - 0.18 ) > (0.45 - 4*( G.g- G.r ) )    )
+          and ((G.g - G.r ) > ( 1.35 + 0.25 *( G.r - G.i ) )                     )
+group by G.objID;
+end;
+$$ language plpgsql;
+
